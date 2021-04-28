aca968754bb31cb7e5c347a12440c64d38f5bca0
JEFFGULICK <jeffgulick@gmail.com>
2021-04-27 22:56:03 -0500

    working on register route

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { check, validationResult } = require('express-validator/check');
+const { check, validationResult } = require('express-validator');
+const gravatar = require('gravatar');
+
+const User = require('../../models/User');
 
 //@route  api/users
 //@desc   user register route
@@ -9,20 +12,52 @@ router.post(
   '/',
   //backend user input validation using express-validator
   [
-    check('name', 'Name is Required').not().isEmpty(),
-    check('email', 'Please enter a valid email').isEmail(),
+    check('name', 'Name is Required').not().isEmpty().trim(),
+    check('email', 'Please enter a valid email')
+      .isEmail()
+      .normalizeEmail()
+      .trim(),
     check(
       'password',
       'Please enter a password with 6 or more characters'
     ).isLength({ min: 6 }),
   ],
-  (req, res) => {
+  async (req, res) => {
     //checking for errors, sends validation error details if any above validation rules are broken
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    res.send('Users Test Route');
+
+    //destructuring request body object
+    const { name, email, password } = req.body;
+
+    try {
+      //checks for existing email in db
+      const user = await User.findOne({ email: email });
+      if (user) {
+        res.status(400).json({ errors: { msg: 'user already exists' } });
+      }
+      //grabs gravatar if one is available
+      const profileImage = gravatar.url(email, { s: '200', r: 'pg', d: 'mm' });
+      //creating new database instance
+      user = new User({
+        name,
+        email,
+        password,
+        profileImage,
+      });
+      // needed to complete register route
+      //see if user exists
+      //get gravatar
+      //encrypt password
+      //return json webToke
+
+      res.send(name);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send('server error');
+    }
   }
 );
 
