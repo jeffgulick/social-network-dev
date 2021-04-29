022de25b6ccd944c89048b6b5e2df26e88d100d5
JEFFGULICK <jeffgulick@gmail.com>
2021-04-28 22:43:34 -0500

    encrypted user password, can also save user to db

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const { check, validationResult } = require('express-validator');
 const gravatar = require('gravatar');
+const normalize = require('normalize-url');
+const bcrypt = require('bcryptjs');
 
 const User = require('../../models/User');
 
@@ -12,7 +14,7 @@ router.post(
   '/',
   //backend user input validation using express-validator
   [
-    check('name', 'Name is Required').not().isEmpty().trim(),
+    check('name', 'Name is Required').not().isEmpty().trim().toUpperCase(),
     check('email', 'Please enter a valid email')
       .isEmail()
       .normalizeEmail()
@@ -33,29 +35,37 @@ router.post(
     const { name, email, password } = req.body;
 
     try {
-      //checks for existing email in db
-      const user = await User.findOne({ email: email });
+      //checks for existing user saved in db
+      let user = await User.findOne({ email: email });
       if (user) {
-        res.status(400).json({ errors: { msg: 'user already exists' } });
+        res.status(400).json({ errors: [{ msg: 'user already exists' }] });
       }
+
       //grabs gravatar if one is available
-      const profileImage = gravatar.url(email, { s: '200', r: 'pg', d: 'mm' });
-      //creating new database instance
+      const avatar = normalize(
+        gravatar.url(email, { s: '200', r: 'pg', d: 'mm' }),
+        { forceHttps: true }
+      );
+
+      // creating new user object to save to db
       user = new User({
         name,
         email,
+        avatar,
         password,
-        profileImage,
       });
-      // needed to complete register route
-      //see if user exists
-      //get gravatar
-      //encrypt password
-      //return json webToke
 
-      res.send(name);
+      //creating salt round and then hashing/encrypting password
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+
+      //saving user object with hashed password to db
+      await user.save();
+      res.send(`User, ${name} is now registered`);
+
+      //return json webToke
     } catch (error) {
-      console.error(error.message);
+      console.error(error);
       res.status(500).send('server error');
     }
   }
